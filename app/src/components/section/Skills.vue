a88734c93f34406a7334c15e70491a62b9835a5e
A Parody G <aparodyg@gmail.com>
2022-01-10 17:16:18 +0100

    responsive: Skills section

--- app/src/components/section/Skills.vue
+++ app/src/components/section/Skills.vue
@@ -1,62 +1,79 @@
 <template>
     <section id="Skills" :class="'section'">
         <div :class="'container'">
-
             <div :class="'row'">
 
-                <div style="flex: 0 0 100%; padding-bottom: 1rem; font-size: 20px">
+                <div :class="'col-12'" style="padding-bottom: 1rem; font-size: 20px">
                     <h1>Skills</h1>
                 </div>
 
-                <div style="flex: 0 0 33.33%; padding-bottom: 3.5rem">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/python.png')" :height="45"/>
                     <h3>Python</h3>
                     <p>Flask, Django, SQLAlchemy, pandas, numpy, openpyxl</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/vuejs.png')" :height="45"/>
                     <h3>Frontend</h3>
                     <p>HTML, CSS, JavaScript, Vue.js, Nuxt</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/aws.png')" :height="45"/>
                     <h3>Cloud engineering</h3>
                     <p>AWS EC2, AWS Lambda, AWS SQS, Elastic Beanstalk, S3, CloudFormation, CloudWatch</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%; padding-bottom: 3.5rem">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/nodejs.png')" :height="45"/>
                     <h3>Node.js</h3>
                     <p>Express</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/database.png')" :height="45"/>
                     <h3>Databases</h3>
                     <p>MySQL, SQLite, AWS DynamoDB, MongoDB</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/gitlab.png')" :height="45"/>
                     <h3>DevOps</h3>
                     <p>Gitlab CI/CD</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/git.png')" :height="45"/>
                     <h3>Tech Tools</h3>
                     <p>git, serverless framework, jupyter notebook</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/scrum.png')" :height="45"/>
                     <h3>Tools</h3>
                     <p>Slack, Asana, Nuclino, Excel, SCRUM...</p>
                 </div>
 
-                <div style="flex: 0 0 33.33%">
+                <div 
+                    :class="'col-12 col-md-6 col-lg-4'">
+                    
                     <img :src="require('@/assets/icon/learn.png')" :height="45"/>
                     <h3>Mindset</h3>
                     <p>keep on learning</p>
@@ -79,14 +96,34 @@ export default {
         display: flex; justify-content: center; align-items: center;
         background: #eaf2fa;
     }
+    
     .row {
-        display: flex;
-        flex-wrap: wrap;
-        text-align: center;
+        display: flex; flex-wrap: wrap; text-align: center;
     }
+    
+    .col-12 {
+        flex: 0 0 100%;
+        max-width: 100%;
+    }
+
+    @media only screen and (min-width: 576px) {
+        .col-md-6 {
+            flex: 0 0 50%;
+            max-width: 50%
+        }
+    }
+
+    @media only screen and (min-width: 768px) {
+        .col-lg-4 {
+            flex: 0 0 33.33333333%;
+            max-width: 33.33333333%;
+        }
+    }
+
     img {
         padding-bottom: .5rem;
     }
+    
     h3 {
         margin-top: 0;
         margin-bottom: 5px;
@@ -96,6 +133,7 @@ export default {
         font-style: italic;
         font-weight: 600;
     }
+    
     p {
         font-size: 14px;
         margin: 0;
